ae5967d080ebc4a5d04690dfb51a9c86364273ea
arinaroy7 <royarina125@gmail.com>
2024-10-11 17:58:26 +0400

    Made edits

--- Ball№1/ProgramBall.cs
+++ Ball№1/ProgramBall.cs
@@ -1,5 +1,5 @@
 ﻿using ImGuiNET;
-using SixLabors.ImageSharp; // умеет в памяти создавать изображения сама
+using SixLabors.ImageSharp; 
 using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
@@ -10,30 +10,22 @@ partial class Program
 {
   const int WD=200;
   const int HG=200;
+
   float currentX = 100f;
   float currentY = 100f;
-  Image<Rgba32> img = new Image<Rgba32>(WD, HG);
-  nint imgid=0;
   float ballX, ballY;
   float speedX = 2;
   float speedY = 1.5f; 
   float ballRadius = 20;
+
+  Image<Rgba32> img = new Image<Rgba32>(WD, HG);
+  nint imgid=0;
 
   void Draw() 
   {
     Brush fone = Brushes.Solid(Color.White);
     img.Mutate(x => x.Fill(fone));
     img.Mutate(x => x.Fill(Brushes.Solid(Color.Green), new EllipsePolygon(ballX, ballY, ballRadius)));
-
-    /*img.Mutate(x => x
-            .Fill(fillBrush, SixLabors.ImageSharp.Drawing.Primitives.Circle.Create(currentX, currentY, 30)) 
-            .Draw(outlinePen, SixLabors.ImageSharp.Drawing.Primitives.Circle.Create(currentX, currentY, 30)) 
-            );
-            -------------
-    img.Mutate(x => x
-      .Fill(fillBrush, SixLabors.ImageSharp.Drawing.Primitives.Ellipse.Create(currentX, currentY, radius, radius)) 
-      .Draw(outlinePen, SixLabors.ImageSharp.Drawing.Primitives.Ellipse.Create(currentX, currentY, radius, radius)) 
-    );*/
   }
   void MovementBall()
   {
@@ -42,7 +34,7 @@ partial class Program
     if (ballX - ballRadius < 0)
     {
         ballX = ballRadius; 
-        speedX = -speedX;   
+        speedX = -speedX;   //смена направления движения 
     }
     if (ballX + ballRadius > WD)
     {
@@ -62,13 +54,15 @@ partial class Program
   }
   protected override void Render()
   {
-    ImGui.Begin("Win");   
-    Draw();
+    ImGui.Begin("Win");
     if (ImGui.Button("Выход")) Close();
+
+    Draw();
     MovementBall();
+    ImGui.End();
+
     RemoveImage("img"); 
     AddOrGetImagePointer("img", img, false, out imgid); //загружаем новую текстуру
     ImGui.Image(imgid, new (WD,HG)); //отображаем
-    ImGui.End();
   }
 }
